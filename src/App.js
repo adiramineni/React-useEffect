4d79ef2e52ed73204f56c16792ec4c9a27c667bb
adiramineni <adiramineni@gmail.com>
2023-09-20 18:36:47 +0530

    context API

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
+import AuthContext from './store/auth-context';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   
-  useEffect(() => {
+  useEffect(() => {   
     const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
     
     if(storedUserLoggedInInformation === '1') {
@@ -28,13 +29,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
+      <AuthContext.Provider value={{isLoggedIn: isLoggedIn, onLogout: logoutHandler }}>
+        <MainHeader />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
   );
 }
 
